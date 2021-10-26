6b74e85ec277fee4de6fa7a3b5679edcde2fd11e
Bernardo <bernardoafonso9734@gmail.com>
2021-10-26 16:02:02 -0300

    Criei funcoes de sacar e depositar e mais uma conta

--- contas.js
+++ contas.js
@@ -1,22 +1,55 @@
+//criando uma classe de pessoa
 class Pessoa{
     nome 
     idade
     cpf
 }
+//criando uma classe de conta
 class Conta{
     codigo
     cliente
     data
+    saldo = 0
+    //criando uma funcao para depositar dinheiro na conta
+    depositar(valor) {
+        this.saldo += valor;
+    }
+    //criando uma funcao de saque
+    sacar(valor){
+        if(valor > this.saldo){
+            console.log("Saldo insuficiente para realizar o saque!");
+        }else{
+            this.saldo -= valor;
+            return valor;
+        }
+    }
 }
+//adicionando informacoes a pessoa 1
 const Pessoa1 = new Pessoa;
 Pessoa1.nome = "Bernardo Afonso";
 Pessoa1.idade = 16;
 Pessoa1.cpf = "01847131611";
-console.log(Pessoa1);
+//adicionando informacoes a conta da pessoa 1
+const Conta1 = new Conta;
+Conta1.codigo = "9734";
+Conta1.cliente =Pessoa1;
+Conta1.data = "26-10-2021"
+Conta1.depositar(500);
+Conta1.sacar(300);
 
-const Pessoa1Conta = new Conta;
-Pessoa1Conta.codigo = "9734";
-Pessoa1Conta.cliente =Pessoa1;
-Pessoa1Conta.data = "26-10-2021"
+//adicionando informacoes a pessoa 2
+const Pessoa2 = new Pessoa;
+Pessoa2.nome = "Arthur Silva";
+Pessoa2.idade = "21";
+Pessoa2.cpf = "16254788943";
+//adicionando informacoes a conta da pessoa 2
+const Conta2 = new Conta;
+Conta2.codigo ="4454";
+Conta2.cliente = Pessoa2;
+Conta2.data ="26-10-2021";
+Conta2.depositar(100);
 
-console.log(Pessoa1Conta);
+console.log("Conta do Bernardo:");
+console.log(Conta1);
+console.log("Conta do Arthur:");
+console.log(Conta2);
