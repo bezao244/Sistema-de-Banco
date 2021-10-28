e47c45dcddfa4540860a8ed3dd83b041b92c6efb
Bernardo <bernardoafonso9734@gmail.com>
2021-10-28 14:59:32 -0300

    Adicionei um construtor e um novo cliente

--- contas.js
+++ contas.js
@@ -3,6 +3,13 @@ class Pessoa{
     nome 
     idade
     cpf
+
+    //criando um construtor para melhorar as atribuicoes as classes
+    constructor(nome, idade, cpf){
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
 }
 //criando uma classe de conta
 class Conta{
@@ -16,6 +23,7 @@ class Conta{
     }
     //criando uma funcao de saque
     sacar(valor){
+        //validando se o valor que a pessoa quer sacar, tem saldo suficiente
         if(valor > this.saldo){
             console.log("Saldo insuficiente para realizar o saque!");
         }else{
@@ -23,25 +31,27 @@ class Conta{
             return valor;
         }
     }
+    //funcao de tranferir
+    transferir(valor, conta){
+        //criando uma const para salvar o valor que vai ser transferido para ser ultilizado no deposito
+        const valorSacado =this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
 //adicionando informacoes a pessoa 1
-const Pessoa1 = new Pessoa;
-Pessoa1.nome = "Bernardo Afonso";
-Pessoa1.idade = 16;
-Pessoa1.cpf = "01847131611";
+const Pessoa1 = new Pessoa("Bernardo Afonso", "16", "01847131611");
+
 //adicionando informacoes a conta da pessoa 1
 const Conta1 = new Conta;
 Conta1.codigo = "9734";
 Conta1.cliente =Pessoa1;
 Conta1.data = "26-10-2021"
 Conta1.depositar(500);
-Conta1.sacar(300);
+Conta1.depositar(300);
 
 //adicionando informacoes a pessoa 2
-const Pessoa2 = new Pessoa;
-Pessoa2.nome = "Arthur Silva";
-Pessoa2.idade = "21";
-Pessoa2.cpf = "16254788943";
+const Pessoa2 = new Pessoa("Arthur Silva", "21", "16254788943");
+
 //adicionando informacoes a conta da pessoa 2
 const Conta2 = new Conta;
 Conta2.codigo ="4454";
@@ -49,7 +59,19 @@ Conta2.cliente = Pessoa2;
 Conta2.data ="26-10-2021";
 Conta2.depositar(100);
 
-console.log("Conta do Bernardo:");
+Conta1.transferir(100, Conta2);
+
+const Pessoa3 = new Pessoa("Samuel Rocha", "19", "11111111111");
+
+const Conta3 = new Conta;
+Conta3.codigo = "3035";
+Conta3.cliente = Pessoa3;
+Conta3.data = "28-10-2021";
+Conta3.depositar(100);
+
+console.log("Conta do ", Pessoa1.nome);
 console.log(Conta1);
-console.log("Conta do Arthur:");
+console.log("Conta do ", Pessoa2.nome);
 console.log(Conta2);
+console.log("Conta do ", Pessoa3.nome);
+console.log(Conta3);
